520999290c76a1fd6a2c67a1a530a0197cb977e0
francotaind <francotaind00@gmail.com>
2023-07-13 09:29:50 -0700

    Cprogramming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*
+ * File: 5-printf.c
+ * Author: julien
+ */
+#include <stdio.h>
+/**
+ * main prints "with proper grammar, but the outcome is a piece of art
+ * Return: always 0
+ */
+int main(void)
+{       printf(with proper grammar, but the outcome is a piece of art,
+	return(0);
+}	
