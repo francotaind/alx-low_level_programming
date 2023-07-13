ee117ca6efeddea5c42c292dc293e1a5daa379b0
francotaind <francotaind00@gmail.com>
2023-07-13 10:46:31 -0700

    Cprogramming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  */
 #include <stdio.h>
 /**
- * main prints "with proper grammar, but the outcome is a piece of art"
+ * main- prints "with proper grammar, but the outcome is a piece of art"
  * Return: always 0
  */
 int main(void)
 {      
        	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }	
