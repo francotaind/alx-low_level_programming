a11073d8ef733836cec741e7ba71c684a96e809f
francotaind <francotaind00@gmail.com>
2023-07-13 08:38:53 -0700

    Cprogramming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
 #include <stdio.h>
 
-int main(void) 
-{ 
-	puts("\"Programming is like building a multilingual puzzle);
-	return(0);
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
+	return (0);
 }
