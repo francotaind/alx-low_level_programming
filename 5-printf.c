74342fd1b8e100e900afca798952d39749d07c28
francotaind <francotaind00@gmail.com>
2023-07-13 09:41:29 -0700

    Cprogramming

---
+++ 5-printf.c
@@ -0,0 +1,14 @@
+/*
+ * File: 5-printf.c
+ * Author: francota
+ */
+#include <stdio.h>
+/**
+ * main "prints with proper grammar, but the outcome is a piece of art
+ * return: always 0
+ */
+int main(void)
+{
+   printf("with proper grammar, but the outcome is a piece of art,\n")
+   return(0);
+}   
