6c8f47b67b82160492234a5004d06ccc8ecd31f5
francotaind <francotaind00@gmail.com>
2023-07-18 11:39:41 -0700

    Cprogramming

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes absolute values of an integer
+ * @i: input number as an interger
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+	return (i);
+	}
+	else
+	{
+	return (i * -1);
+	}
+}
