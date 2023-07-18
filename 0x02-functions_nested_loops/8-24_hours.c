8cf439c1107f900c3f35ce8112653738cf981f74
francotaind <francotaind00@gmail.com>
2023-07-18 13:31:29 -0700

    Cprogramming

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * jack_bauer - prints the minutes of the day
- *
  * Return: no return
  */
 void jack_bauer(void)
 {
 	int a, b, c, d;
+
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -15,9 +15,9 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-				       if (a >= 2 && b >= 4)
-				       break;
-		 			_putchar(a + 48);
+					if (a >= 2 && b >= 4)
+						break;
+					_putchar(a + 48);
 					_putchar(b + 48);
 					_putchar(58);
 					_putchar(c + 48);
@@ -27,5 +27,4 @@ void jack_bauer(void)
 			}
 		}
 	}
-
-}	
+}
