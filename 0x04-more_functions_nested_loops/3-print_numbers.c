ba0db22d88c74287ab2314ee78ad5e6795fb7c77
francotaind <francotaind00@gmail.com>
2023-07-20 08:29:23 -0700

    Cprogramming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_numbers - prints out numbers
+*Description: prints out 0-9
+*Return: always 0
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for ( i = 0; i <= 9; i++)
+	
+{	_putchar(i + '0');
+	
+}
+	_putchar('\n');
+}
