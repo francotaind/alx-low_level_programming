d28c8bfce82c5ccfa4816777857da5af13fca84c
francotaind <francotaind00@gmail.com>
2023-07-20 09:28:39 -0700

    Cprogramming

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*more_numbers - print numbers x14
+*Description: prints 10 times numbers from 0-14
+*Return: always 0 success
+*/
+void more_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+	for (int j = 0; j <= 14; j++)
+	{
+	if (j > 9)
+	{
+	_putchar('0' + j / 10);
+	}
+	_putchar('0' + j % 10);
+	}
+	_putchar('\n');
+	}
+}
+
+
