fb237480dfc424f268f7e79ba921cb4f66df5c03
francotaind <francotaind00@gmail.com>
2023-07-20 06:29:18 -0700

    Cprogramming

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
-*mul(int a, int b) - multiplies two intergers
-*@int
+*mul - multiplies two intergers
+*@a: integer
+*@b: integer
 *Return: 0 always success
 *Description: multiplies two integers
 */
 int mul(int a, int b)
 {
 	int c = a * b;
+
 	return (c);
 }
