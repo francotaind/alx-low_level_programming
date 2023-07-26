bb6d5386e925ff99bc5d17dead36f442f771fe4f
francotaind <francotaind00@gmail.com>
2023-07-26 10:16:37 -0700

    pointers and arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
 *_strncat - concatenates two strings
-*
+*@src: the source string
+*@dest: the destination string
+*@n: the number of index space
+*Return: dest string
 */
 char *_strncat(char *dest, char *src, int n)
 {
 	int j = 0;
-
 	int i = 0;
 
 	while (j < n && src[j] != '\0')
@@ -14,10 +16,6 @@ char *_strncat(char *dest, char *src, int n)
 	dest[i] = src[j];
 	i++, j++;
 	}
-
-
-	dest[i] = '\0';	
+	dest[i] = '\0';
 	return (dest);
-	 
-
 }
