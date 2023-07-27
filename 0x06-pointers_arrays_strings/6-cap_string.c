ac72f391549d8cc58fd58ecb22de7477daf87365
francotaind <francotaind00@gmail.com>
2023-07-27 03:17:17 -0700

    pointers and arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,24 @@
 #include "main.h"
 #include <ctype.h>
 /**
-*cap_string - 
-*
-*
-*Return: always 0
+*cap_string -capitalizes words in a string
+*@capitalize_next capitalize next word in a string
+*@str: string to be corrected
+*Return: str
 */
 char *cap_string(char *str)
 {
 	int capitalize_next = 1;
-	
+
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
+
 	char c = str[i];
-	if (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';'||
-	c == '.'|| c == '!' || c == '?' || c == '"' || c == '(' || c == ')' ||
+
+	if (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' ||
+	c == '.' || c == '!' || c == '?' || c == '"' || c == '(' || c == ')' ||
 	c == '}' || c == '}')
 	{
 	capitalize_next = 1;
@@ -31,5 +33,4 @@ char *cap_string(char *str)
 	}
 	}
 	return (str);
-
 }
