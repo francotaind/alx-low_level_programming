455b56850689a9b661f24f8c68c642ed556482f7
francotaind <francotaind00@gmail.com>
2023-08-02 15:05:47 -0700

    Recursion tasks

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_strlen_recursion - returns the length of a string
+*@str: pointer to the string
+*Return: length
+*/
+int _strlen_recursion(char *str)
+{
+	int length = 0;
+
+	for (; (str[length] = !'\0'); length++);
+	{
+	}
+	return (length);
+}
