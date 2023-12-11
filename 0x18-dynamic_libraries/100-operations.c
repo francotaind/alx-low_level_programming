d15b79146e839addd36dffe9515abd4585a72876
francotaind <francotaind00@gmail.com>
2023-12-11 20:26:43 +0300

    shellscripts

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
-int add(int a, int b)
-	{
-	return a + b;
-	}
+// Function to add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function to subtract two integers
+int sub(int a, int b) {
+    return a - b;
+}
+
+// Function to multiply two integers
+int mul(int a, int b) {
+    return a * b;
+}
+
+// Function to divide two integers
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Error: Division by zero\n");
+        return 0;  // You might want to handle this case differently in a real application
+    }
+}
+
+// Function to calculate the remainder of the division
+int mod(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    } else {
+        printf("Error: Modulo by zero\n");
+        return 0;  // You might want to handle this case differently in a real application
+    }
+}
+
